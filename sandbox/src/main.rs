1731d6336a0ad039dfbb7a7d69faf44748647550
Nurahmadie <nurahmadie@gmail.com>
2018-12-02 01:53:24 +0700

    Capacitive touch demo

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -3,6 +3,7 @@
 
 extern crate cortex_m_rt;
 extern crate panic_halt;
+extern crate embedded_hal;
 extern crate tomu_hal;
 
 use cortex_m_rt::entry;
@@ -10,7 +11,10 @@ use tomu_hal::{
     peripherals,
     toboot_config,
     led::LedTrait,
+    gpio::{pin, OpenDrain, InputPullDown},
 };
+
+use embedded_hal::digital::{OutputPin, InputPin};
 
 /// this works too:
 /// ```
@@ -27,10 +31,34 @@ toboot_config! {
 fn main() -> ! {
     let mut p = peripherals::take();
 
+    let pin_c0      = p.gpio.split::<pin::C0<InputPullDown>>();
+    let mut pin_e12 = p.gpio.split::<pin::E12<OpenDrain>>();
+
     p.watchdog.disable();
 
-    p.led.green().on();
-    p.led.red().off();
+    p.led.green().off();
+    p.led.red().on();
 
-    loop {}
+    pin_e12.set_high();
+
+    let mut counter = 0;
+
+    loop {
+        if pin_c0.is_high() {
+            pin_e12.set_low();
+            p.led.green().on();
+            p.led.red().off();
+            counter = 2000000;
+        }
+
+        if counter > 0 {
+            counter = counter - 1;
+        }
+
+        if counter == 0 {
+            pin_e12.set_high();
+            p.led.green().off();
+            p.led.red().on();
+        }
+    }
 }
